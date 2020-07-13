2ea9de81cfb93fb9540003d9ea7ce423462d2ac9
JC <jcmpes@gmail.com>
2020-07-13 18:48:00 +0200

    Added models

---
+++ auctions/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+def user_directory_path(instance, filename):
+    # File will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Auction(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    desctiption = models.TextField(blank=False)
+    image = models.ImageField(upload_to=user_directory_path)
+
+
+class Bid(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    auction_id = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    date = models.DateTimeField(default=timezone.now)
+
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=60)
+    comment = models.TextField(blank=False)
